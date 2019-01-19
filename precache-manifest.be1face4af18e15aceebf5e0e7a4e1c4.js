e1c40343f5deaeb1db26289779a24d63b8806772
Ryan Santos <ryansantos86@gmail.com>
2019-01-18 23:57:20 -0800

    Updates

---
+++ precache-manifest.be1face4af18e15aceebf5e0e7a4e1c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3271800f36239a40dff",
+    "url": "/restaurant_menu/static/css/main.abc91546.chunk.css"
+  },
+  {
+    "revision": "d3271800f36239a40dff",
+    "url": "/restaurant_menu/static/js/main.d3271800.chunk.js"
+  },
+  {
+    "revision": "65273cf4a7ac7503495f",
+    "url": "/restaurant_menu/static/js/1.65273cf4.chunk.js"
+  },
+  {
+    "revision": "0a1b5f29e481af482e1b",
+    "url": "/restaurant_menu/static/js/runtime~main.0a1b5f29.js"
+  },
+  {
+    "revision": "26fdb364830d620b1f27c54e60f6e8eb",
+    "url": "/restaurant_menu/static/media/hamburger-menu-icon.26fdb364.svg"
+  },
+  {
+    "revision": "4ad7f624e6b3f7c6cb564d706219362b",
+    "url": "/restaurant_menu/static/media/github.4ad7f624.svg"
+  },
+  {
+    "revision": "81ca778f914864ed61c9ff102b110404",
+    "url": "/restaurant_menu/static/media/38b92ac3b861e8a754d0131437fe56eb.81ca778f.svg"
+  },
+  {
+    "revision": "98188e642554fa7deafb0cdee54ff928",
+    "url": "/restaurant_menu/static/media/facebook.98188e64.svg"
+  },
+  {
+    "revision": "181e7f1daa97dfcfff367e626cf731d9",
+    "url": "/restaurant_menu/static/media/instagram-icon.181e7f1d.svg"
+  },
+  {
+    "revision": "e59fc66a25a464453007614ef7c7c4af",
+    "url": "/restaurant_menu/static/media/yelp.e59fc66a.svg"
+  },
+  {
+    "revision": "509a348b3bca13d5cf7731386cc8f449",
+    "url": "/restaurant_menu/index.html"
+  }
+];
