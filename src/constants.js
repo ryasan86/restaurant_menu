6376bc6557e693c9ddb986a5e1efd964f5024b33
Ryan Santos <ryansantos86@gmail.com>
2019-01-19 16:36:22 -0800

    refactor

--- src/constants.js
+++ src/constants.js
@@ -1,27 +1,30 @@
 const FOODS = [
   {
+    name: 'CHICKEN BURRITO',
+    imgUrl: 'https://i.postimg.cc/3rB1qvK8/burrito-chicken-close-up-461198.jpg'
+  },
+  {
     name: 'STEAK',
     imgUrl: 'https://i.postimg.cc/02yn1yQp/beef-cooking-delicious-769289.jpg'
   },
   {
     name: 'CLUB SANDWICH',
-    imgUrl: 'https://i.postimg.cc/8kWVKMXF/bread-breakfast-burlap-1600711-scaled.jpg'
+    imgUrl:
+      'https://i.postimg.cc/8kWVKMXF/bread-breakfast-burlap-1600711-scaled.jpg'
   },
   {
     name: 'BACON CHEESEBURGER',
     imgUrl: 'https://i.postimg.cc/xTpRfVJB/burger-chips-dinner-70497.jpg'
   },
   {
-    name: 'CHICKEN BURRITO',
-    imgUrl: 'https://i.postimg.cc/3rB1qvK8/burrito-chicken-close-up-461198.jpg'
-  },
-  {
     name: 'FISH FILLET',
-    imgUrl: 'https://i.postimg.cc/Vkkp8CgW/close-up-cooking-dinner-46239-scaled.jpg'
+    imgUrl:
+      'https://i.postimg.cc/Vkkp8CgW/close-up-cooking-dinner-46239-scaled.jpg'
   },
   {
     name: 'CHOCOLATE CAKE',
-    imgUrl: 'https://i.postimg.cc/Xq6tS11r/cake-chocolate-chocolate-cake-132694-scaled.jpg'
+    imgUrl:
+      'https://i.postimg.cc/Xq6tS11r/cake-chocolate-chocolate-cake-132694-scaled.jpg'
   }
 ];
 
